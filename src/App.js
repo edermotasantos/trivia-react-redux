9f0761fb1a9f2c88ca635e65560bb0878f8581f4
Milton Castro <castro.milton07@gmail.com>
2021-08-03 20:52:59 -0300

    Requisito 1 - Detalhes do Lint

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src= { logo } className="App-logo" alt="logo" />
+          <img src={ logo } className="App-logo" alt="logo" />
           <p>SUA VEZ</p>
           <Login />
         </header>
