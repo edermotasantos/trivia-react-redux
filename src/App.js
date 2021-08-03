003a51b57c623a292a1c3d72d3157e4adf600520
Guilherme Curi <82242295+gscuri@users.noreply.github.com>
2021-08-03 20:44:37 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
-import logo from "./trivia.png";
-import "./App.css";
-import Login from "./pages/Login";
+import React from 'react';
+import logo from './trivia.png';
+import './App.css';
+import Login from './pages/Login';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src= { logo } className="App-logo" alt="logo" />
           <p>SUA VEZ</p>
           <Login />
         </header>
